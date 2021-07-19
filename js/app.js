39d6a43abfbff417eecda4126afcbb6d697e50eb
zakaria Shahen <zakariya.shahen@gmail.com>
2021-07-19 02:39:13 +0200

    Edit

--- js/app.js
+++ js/app.js
@@ -22,11 +22,13 @@ const mainSelector = document.querySelector('main');
 const countSection = mainSelector.querySelectorAll('section').length; // get total number section
 const fragment = document.createDocumentFragment();
 let hasSectionActive = '#section1';
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
 function createLiNave() { 
     
     // loop create section 
@@ -47,9 +49,51 @@ function createLiNave() {
 }
 
 
+// change active 'a' tag/Element by scroll
+function aActiveChangeScroll(event){
+    // for (let i = 0; i < countSection; i++){
+        // const section = document.querySelectorAll('section')[0];
+        // const positionSection = section.getBoundingClientRect();
+        // const positionUser = window.getBoundingClientRect();
+        // console.log(positionSection);
+        
+    // }
+    // console.log(event);
+    
+    showNavbar();
+
+}
+
+function showNavbar(event = 0) {
+    navSelector.parentElement.style.display = 'block';
+    const n = setTimeout(hiddenNavbar, 7000);
+    if (event !== 0){
+        clearTimeout(n);
+
+    }
+    console.log(event);
+}
 
 
-// change active section and a tag 
+function hiddenNavbar() {
+    navSelector.parentElement.style.display = 'none';
+}
+
+// change active a tag/Element
+function activeAElement(aElementNew) {
+    const lisActiveNow = document.querySelector('a[href="' + hasSectionActive + '"]');
+    const lisActiveNew = document.querySelector('a[href="' + aElementNew + '"]');
+
+    // clear active a tag 
+    lisActiveNow.classList.remove('active');
+
+    // add class active a tag
+    lisActiveNew.classList.add('active');
+}
+
+
+
+// change active section  
 function active(event) {
 
     const eventId = event.srcElement.hash;
@@ -57,24 +101,18 @@ function active(event) {
         // remove class active 
         const sectionActiveNow = document.querySelector(hasSectionActive);
         sectionActiveNow.removeAttribute('class');
-        const lisActiveNow = document.querySelector('a[href="' + hasSectionActive + '"]');
-
-        // clear active a tag 
-        lisActiveNow.className = 'menu__link';
-
 
         // add class active
         const sectionActiveNew = document.querySelector(eventId);
         sectionActiveNew.className = 'your-active-class'; 
 
-
+        activeAElement(eventId);  
         hasSectionActive = eventId; // change active save
-    }  // else  ==> exit if 
+    } else {
+        activeAElement(eventId);
+    }
     
-    // add class active a tag
-    const lisActiveNew = document.querySelector('a[href="' + eventId + '"]');
-    lisActiveNew.className = 'menu__link active';
-
+   
 }
     
 /**
@@ -87,8 +125,11 @@ function active(event) {
 
 
 // build the nav
-createLiNave();
-navSelector.addEventListener("click", active)
+
+createLiNave(); // create li an a tags/Element
+
+navSelector.addEventListener("click", active); // Event Click a tag nav bar;
+navSelector.parentElement.addEventListener("mouseenter", showNavbar); // Event Click a tag nav bar;
 
 
 
@@ -107,6 +148,9 @@ navSelector.addEventListener("click", active)
 // Build menu 
 
 // Scroll to section on link click
+window.addEventListener('scroll', aActiveChangeScroll);
+
+
 
 // Set sections as active
 
