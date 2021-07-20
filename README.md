38b8e1a2885f01ca971ae93204bf0a5f363d76d6
zakaria Shahen <zakariya.shahen@gmail.com>
2021-07-20 22:28:14 +0200

    add button up, and end revew

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Landing Page Project
 
-
+# Added
+1- navbar Dynamic changes with Section
+2- Section and Navbar changes to Active When Scroll
+3- The button to go to the top of the page and disappears you have the user at the top of the page 
--- js/app.js
+++ js/app.js
@@ -11,40 +11,135 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 const navSelector = document.querySelector('#navbar__list');
 const mainSelector = document.querySelector('main');
 const countSection = mainSelector.querySelectorAll('section').length; // get total number section
 const fragment = document.createDocumentFragment();
+const divIconUP = document.querySelector('.up__ico__container');
+
 let hasSectionActive = '#section1';
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
-function createLiNave() { 
-    
+// change active a tag (navbar)
+function changeActiveNavbar(activeTo) {
+    const liTagActiveNow = document.querySelector('a[href="' + hasSectionActive + '"]');
+    const changeLiActiveTo = document.querySelector('a[href="' + activeTo + '"]');
+
+    // clear 'active' a tag 
+    liTagActiveNow.classList.remove('active');
+
+    // add class active a tag
+    changeLiActiveTo.classList.add('active');
+}
+
+// change active section  
+function active(event) {
+
+    const eventId = '#' + event.target.id; // get id event
+
+    if (event.target.nodeName === 'HEADER') {
+        // if viewport => header tag 
+
+        // remove class active section 
+        const sectionActiveNow = document.querySelector(hasSectionActive);
+        sectionActiveNow.removeAttribute('class');
+
+        // remove active navbar
+        const lisActiveNow = document.querySelector('a[href="' + hasSectionActive + '"]');
+        lisActiveNow.classList.remove('active');
+
+        hasSectionActive = '#section1';
+
+        // add class active
+        const sectionActiveNew = document.querySelector(hasSectionActive);
+        sectionActiveNew.className = 'your-active-class';
+
+        upTopButtonHidden();
+    } else if (eventId !== hasSectionActive) {
+        // if viewport =>  not header tag and not hasSectionActive 
+
+        // remove class active 
+        const sectionActiveNow = document.querySelector(hasSectionActive);
+        sectionActiveNow.removeAttribute('class');
+
+        // add class active
+        const sectionActiveNew = document.querySelector(eventId);
+        sectionActiveNew.className = 'your-active-class';
+
+        changeActiveNavbar(eventId);
+        hasSectionActive = eventId; // change active save 
+
+        upTopButtonShow();
+
+    } else {
+        // if viewport =>  not header tag and Equal hasSectionActive 
+        changeActiveNavbar(eventId);
+        upTopButtonShow();
+
+    }
+
+
+}
+
+// show and hidden navbar list at Phone view
+function navbarPhoneShowHidden() {
+    const navbarPhone = document.querySelector('.navbar__menu');
+    navbarPhone.classList.toggle('navbar__menu__show');
+
+}
+
+
+
+// hidden up icon button 
+function upTopButtonHidden() {
+    divIconUP.classList.add('up__ico__container__hidden');
+
+}
+
+// show up icon button 
+function upTopButtonShow() {
+    divIconUP.classList.remove('up__ico__container__hidden');
+
+}
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+ */
+
+
+// build the nav
+function createLiNave() {
     // loop create section 
-    for (let i = 0; i <= countSection; i++){
+    for (let i = 0; i <= countSection; i++) {
 
         // create <li> append <a>
         const newElement = document.createElement('li');
         const newATag = document.createElement('a');
 
 
-        if (i === 0){
+        if (i === 0) {
+            // create icon navbar for Phone 
             newATag.href = 'javascript:void(0);';
             newATag.setAttribute('onclick', 'navbarPhoneShowHidden()');
             newATag.className = 'menu__link menu__link__phone las la-bars';
-            // newATag.style = 'font-size: 2.5em; padding: 0.3em;';
 
         } else {
+            // create navbar list <li>
             newATag.href = '#section' + i;
 
             const headerSelector = document.querySelector('#section' + i); // i => number section
@@ -53,7 +148,7 @@ function createLiNave() {
         }
 
         newElement.appendChild(newATag); // <li> append <a>
-        fragment.appendChild(newElement);//  fragment append <li>
+        fragment.appendChild(newElement); //  fragment append <li>
 
     }
 
@@ -61,112 +156,45 @@ function createLiNave() {
 }
 
 
-
-function showNavbar() {
-    navSelector.parentElement.style.display = 'block';
-    // setTimeout(hiddenNavbar, 7000);
-}
-
-
-function hiddenNavbar() {
-    navSelector.parentElement.style.display = 'none';
-}
-
-// change active a tag/Element
-function activeAElement(aElementNew) {
-    const lisActiveNow = document.querySelector('a[href="' + hasSectionActive + '"]');
-    const lisActiveNew = document.querySelector('a[href="' + aElementNew + '"]');
-
-    // clear active a tag 
-    lisActiveNow.classList.remove('active');
-
-    // add class active a tag
-    lisActiveNew.classList.add('active');
-}
-
-
-
-// change active section  
-function active(event) {
-
-    const eventId = '#' + event.target.id;
-    if (event.target.nodeName === 'HEADER') {
-        // remove class active section 
-        const sectionActiveNow = document.querySelector(hasSectionActive);
-        sectionActiveNow.removeAttribute('class');
-
-        // remove active navbar
-        const lisActiveNow = document.querySelector('a[href="' + hasSectionActive + '"]');    
-        lisActiveNow.classList.remove('active');
-    
-    } else if (eventId !== hasSectionActive){
-        // remove class active 
-        const sectionActiveNow = document.querySelector(hasSectionActive);
-        sectionActiveNow.removeAttribute('class');
-
-        // add class active
-        const sectionActiveNew = document.querySelector(eventId);
-        sectionActiveNew.className = 'your-active-class'; 
-
-        activeAElement(eventId);  
-        hasSectionActive = eventId; // change active save 
-    } else {
-        activeAElement(eventId);
-    }
-    
-   
-}
-
-function navbarPhoneShowHidden(){
-    const nav = document.querySelector('.navbar__menu');
-    nav.classList.toggle('navbar__menu__show');
-    
-} 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-
-// build the nav
-
-
-
-
 // Add class 'active' to section when near top of viewport
-let scroll = new IntersectionObserver(function (events){
-    events.forEach(function (event){
-        if (event.isIntersecting){
+let scroll = new IntersectionObserver(function(events) {
+    events.forEach(function(event) {
+        if (event.isIntersecting) {
             active(event);
 
-        } 
-    }); 
- }, {rootMargin: "0% 0% -50% 0%"});
-
-
-
+        }
+    });
+}, { rootMargin: "0% 0% -50% 0%" });
 
 
 
 // Scroll to anchor ID using scrollTO event
-
+// using css ( scroll-behavior: smooth;) 
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 createLiNave();
 
 // Scroll to section on link click
-mainSelector.querySelectorAll('section').forEach(function (section){scroll.observe(section)}); // view section
+mainSelector.querySelectorAll('section').forEach(function(section) { scroll.observe(section) }); // view section
 scroll.observe(mainSelector.querySelector('header')); // view header 
 
-// Set sections as active
 
 
+
+// // option 
+
+// function showNavbar() {
+//     navSelector.parentElement.style.display = 'block';
+//     setTimeout(hiddenNavbar, 7000);
+// }
+
+
+// function hiddenNavbar() {
+//     navSelector.parentElement.style.display = 'none';
+// }
